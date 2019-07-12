00db996b288cf0670da5d6e302ac494766ba9336
John Nguyen <Johnsum41.1@gmail.com>
2019-07-12 10:49:41 -0400

    Updated

---
+++ script/index.js
@@ -0,0 +1,7 @@
+
+const flexStart = document.querySelector('[data-flex-start]');
+
+const flexStartDiv = document.querySelector('[data-flex-start-div]');
+
+flexStart.addEventListener('click', () => 
+    {flexStartDiv.classList.toggle('hidden')});
